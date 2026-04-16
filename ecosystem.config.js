271f335c64a6c273bdd533178c985e328d4b49c9
radheytechmero <radheytechmero@gmail.com>
2026-04-16 11:56:35 +0530

    feat: add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: 'deepgram-twillio',
+      script: 'src/index.ts',
+      interpreter: 'tsx',
+      instances: 1,
+      exec_mode: 'cluster',
+      watch: false,
+      env: {
+        NODE_ENV: 'development',
+        PORT: 443
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      exp_backoff_restart_delay: 100,
+      error_file: 'logs/err.log',
+      out_file: 'logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      merge_logs: true,
+      autorestart: true,
+      max_restarts: 10,
+      min_uptime: '10s',
+      max_memory_restart: '1G'
+    }
+  ]
+};
